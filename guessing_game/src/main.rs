fc6d9db5210a4aae06079768c49aa39298ecc1f1
Oscar Raichert <oscarraichert@hotmail.com>
2023-05-30 19:13:31 -0300

    added error msg

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -20,7 +20,10 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Wrong format, type a number only!");
+                continue;
+            },
         };
 
         println!("You guessed: {guess}");
